4c753122383aa31ba0020eea4696eaa645b3521b
David Luciano Gonçalves Lourenço <david.lourenco11@gmail.com>
2017-10-07 16:48:06 +0100

    Correcting the URL from GETJSON

--- quote generator/javascript.js
+++ quote generator/javascript.js
@@ -3,7 +3,7 @@ function newQuote() {
     $("div").empty();
 
     // call API and get the value of the fields we want to show
-    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(result) {
+    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(result) {
         $.each(result, function(i, field) {
             if (i == "quoteText" || i == "quoteAuthor") {
                 $("div").append(field + " ");
