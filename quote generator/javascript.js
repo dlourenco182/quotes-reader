a7a4360e13001efbdf8b6fe599c47253db312e48
David Luciano Gonçalves Lourenço <david.lourenco11@gmail.com>
2017-10-07 16:42:08 +0100

    Upload API and some image
    Now using API to get the Quote and some image to start looking kind of classy ( while it's  obviously not there yet )

--- quote generator/javascript.js
+++ quote generator/javascript.js
@@ -1,17 +1,13 @@
-var quotes = [
-    'The Way Get Started Is To Quit Talking And Begin Doing - Walt Disney',
-    'The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty - Winston Churchill',
-    'Don’t Let Yesterday Take Up Too Much Of Today - Will Rogers',
-    'You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character - Unknown',
-    'It’s Not Whether You Get Knocked Down, It’s Whether You Get Up – Inspirational Quote By Vince Lombardi',
-    'If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You - Steve Jobs',
-    'People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do - Rob Siltanen',
-    'Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough - Og Mandino',
-    'Entrepreneurs Are Great At Dealing With Uncertainty And Also Very Good At Minimizing Risk. That’s The Classic Entrepreneur - Mohnish Pabrai',
-    'We May Encounter Many Defeats But We Must Not Be Defeated - Maya Angelou'
-]
-
 function newQuote() {
-    var randomNumber = Math.floor(Math.random() * (quotes.length));
-    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+    //clear the div
+    $("div").empty();
+
+    // call API and get the value of the fields we want to show
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(result) {
+        $.each(result, function(i, field) {
+            if (i == "quoteText" || i == "quoteAuthor") {
+                $("div").append(field + " ");
+            }
+        });
+    });
 }
