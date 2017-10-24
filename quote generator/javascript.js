8d33c0dabd39df809cd82c9b977f80335e5d1ed0
David Luciano Gonçalves Lourenço <david.lourenco11@gmail.com>
2017-10-24 23:10:10 +0100

    Some little major update
    Got the guy, got different background and some animation for the quote

--- quote generator/javascript.js
+++ quote generator/javascript.js
@@ -1,13 +1,37 @@
 function newQuote() {
-    //clear the div
-    $("div").empty();
 
+    //animate
+    animateBox();
+
+    //clear the div
+    $("#quoteDisplay").empty();
+
+    animateText();
+
+
+}
+
+function animateBox() {
+    /*  $("#quoteDisplay").height() = 0;
+    $("#quoteDisplay").animate({
+        height: '=180px'
+    }, 250);
+
+*/
+}
+
+function animateText() {
     // call API and get the value of the fields we want to show
     $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(result) {
         $.each(result, function(i, field) {
             if (i == "quoteText" || i == "quoteAuthor") {
-                $("div").append(field + " ");
+                $("#quoteDisplay").append(field + " ");
             }
         });
     });
 }
+
+$("#strat").click(function() {
+    $("#quoteDisplay").animate({ height: 0 }, 250);
+    $("#quoteDisplay").animate({ height: 180 }, 250);
+});
